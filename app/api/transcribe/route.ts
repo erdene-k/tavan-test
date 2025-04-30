841607e27384db26df577cb8cf0c6c4d54ededea
Erdenechuluun Khuderchuluun <tesoro.ec@gmail.com>
2025-04-30 14:01:07 +0800

    KEY

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { SpeechClient } from '@google-cloud/speech';
 
 const speechClient = new SpeechClient({
-  projectId: process.env.GOOGLE_PROJECT_API!,
-  key: process.env.GOOGLE_API_KEY!,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
 }
 );
 
